d6271a526ea47c257a5406fd93adc58415794970
pocket998 <pocket998@gmail.com>
2024-10-02 04:21:22 +0200

    Update KaspaUtils.cs add kaspav2

--- src/Miningcore/Blockchain/Kaspa/KaspaUtils.cs
+++ src/Miningcore/Blockchain/Kaspa/KaspaUtils.cs
@@ -464,13 +464,13 @@ public class KaspaAddressUtility
                 };
 
                 break;
-            case "KASV2":
+            case "KASPAV2":
                 this.stringsToBech32Prefixes = new Dictionary<string, KaspaBech32Prefix>
                 {
-                    { Kasv2CoinConstants.ChainPrefixMainnet, KaspaBech32Prefix.KaspaMain },
-                    { Kasv2CoinConstants.ChainPrefixDevnet, KaspaBech32Prefix.KaspaDev },
-                    { Kasv2CoinConstants.ChainPrefixTestnet, KaspaBech32Prefix.KaspaTest },
-                    { Kasv2CoinConstants.ChainPrefixSimnet, KaspaBech32Prefix.KaspaSim },
+                    { Kaspav2CoinConstants.ChainPrefixMainnet, KaspaBech32Prefix.KaspaMain },
+                    { Kaspav2CoinConstants.ChainPrefixDevnet, KaspaBech32Prefix.KaspaDev },
+                    { Kaspav2CoinConstants.ChainPrefixTestnet, KaspaBech32Prefix.KaspaTest },
+                    { Kaspav2CoinConstants.ChainPrefixSimnet, KaspaBech32Prefix.KaspaSim },
                 };
                 
                 break;
